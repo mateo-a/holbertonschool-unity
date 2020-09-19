e579a72cefbf1d088962a94d5613d1b7d75819ef
mateo-a <alexander.urrego@gmail.com>
2020-09-19 15:20:14 -0500

    [Add] Task 0

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+///<summary>Player Controller</summary>
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 direction = Vector3.zero;
+    private Transform point;
+    public Transform charCam;
+    public float speed = 10f;
+    public float jump = 15f;
+    private float elevation;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        point = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        elevation = direction.y;
+        direction = Vector3.zero;
+        if (Input.GetKey("w"))
+            direction = Vector3.forward + direction;
+        if (Input.GetKey("s"))
+            direction = Vector3.back + direction;
+        if (Input.GetKey("d"))
+            direction = Vector3.right + direction;
+        if (Input.GetKey("a"))
+            direction = Vector3.left + direction;
+        direction = ((charCam.right * direction.x) + (charCam.forward * direction.z)) * speed;
+        if (controller.isGrounded)
+        {
+            if (Input.GetButton("Jump"))
+                elevation = jump;
+            else
+                elevation = 0;
+        }
+        direction.y = elevation;
+        direction.y = direction.y - (20 * Time.deltaTime);
+        if (point.position.y < -20.0f)
+            direction = Vector3.zero;
+        controller.Move(direction * Time.deltaTime);
+        if (point.position.y < -20.0f)
+            point.position = new Vector3(0, 10, 0);
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/Timer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        string seconds = (time % 60).ToString("00.00");
+        string minutes = Mathf.Floor((time % 3600) / 60).ToString();
+        TimerText.text = minutes + ":" + seconds;
+    }
+}
