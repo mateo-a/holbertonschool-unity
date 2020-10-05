11bdbf2ff2828e824a2a279b8357d4b2df244ff8
mateo-a <alexander.urrego@gmail.com>
2020-10-04 19:08:43 -0500

    [Add] Tasks 2-3

---
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CutsceneController : MonoBehaviour
+{
+    private Animator intro01;
+    public GameObject[] sceneStart;
+    void Start()
+    {
+        intro01 = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (intro01.GetCurrentAnimatorStateInfo(0).IsName("Intro01") && intro01.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
+        {
+            StartCoroutine(transitions());
+        }
+    }
+
+    IEnumerator transitions()
+    {
+        foreach (GameObject go in sceneStart)
+        {
+            if (go.activeInHierarchy == false)
+            {
+                go.SetActive(true);
+            }
+            if (go.GetComponent<PlayerController>() != null)
+            {
+                go.GetComponent<PlayerController>().enabled = true;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        gameObject.SetActive(false);
+        yield return new WaitForSeconds(1f);
+    }
+}
