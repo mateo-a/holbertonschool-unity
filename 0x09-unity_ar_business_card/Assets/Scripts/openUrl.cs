32025d02dbc408d2ba76e6d922510a3dac4632d8
mateo-a <alexander.urrego@gmail.com>
2020-11-04 00:08:44 -0500

    [Add] Tasks

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/openUrl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class openUrl : MonoBehaviour
+{
+    void Start(){}
+    void Update(){}
+    public void twitter()
+    {
+        Application.OpenURL("https://twitter.com/mateo_a");
+    }
+
+    public void github()
+    {
+        Application.OpenURL("https://github.com/mateo-a");
+    }
+
+    public void linkedin()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/john-alexander-urrego/");
+    }
+
+    public void email()
+    {
+        Application.OpenURL("mailto:1220@holbertonschool.com");
+    }
+}
---
+++ 0x09-unity_ar_business_card/README.md
@@ -0,0 +1,9 @@
+# 0x09. Unity - AR Business Card
+## General
+
+- What is Vuforia and what are its advantages and disadvantages.
+- What is marker-based augmented reality
+- How to choose or create images optimized for target detection.
+- How to track an image using Vuforia.
+- How to publish applications for both Android and iOS with Vuforia.
+- How to design for an augmented reality environment.
